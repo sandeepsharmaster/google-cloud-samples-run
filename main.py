a233889d5cadc51780ef0cd31524131c29b5b2f1
SHARMA Sandeep <sandeep.sharma@soprasteria.com>
2022-06-19 13:29:57 +0530

    added details of Cloud Vision API

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
-
 from flask import Flask
+
+import sys
+from flask import escape
+from google.cloud import storage
+
 
 app = Flask(__name__)
 
@@ -8,8 +12,79 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
+    file_cont = download_blob(bucket_name="poc-input-bucket-sandy", source_blob_name="Microsoft.png")
+
+    try:
+        print("Calling Image API ")
+        detect_logos_uri('gs://poc-input-bucket-sandy/' + str("Microsoft.png"))
+    except:
+        print("An exception occurred block 2")
+
+    upload_blob_from_memory("poc-output-bucket-sandy", file_cont, "Microsoft.png" + '_processed')
     return "Hello {}!".format(name)
 
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+
+def download_blob(bucket_name, source_blob_name):
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+
+
+    blob = bucket.blob(source_blob_name)
+
+    file_cont = blob.download_as_string()
+    print(
+        "Downloaded storage object {} from bucket {} to local file {}.".format(
+            source_blob_name, bucket_name, file_cont
+        )
+    )
+    return file_cont
+    
+
+def detect_logos_uri(uri):
+    """Detects logos in the file located in Google Cloud Storage or on the Web.
+    """
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image()
+    image.source.image_uri = uri
+
+    response = client.logo_detection(image=image)
+    logos = response.logo_annotations
+    print('Logos:')
+
+    for logo in logos:
+        print(logo.description)
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+def upload_blob_from_memory(bucket_name, contents, destination_blob_name):
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_string(contents)
+
+    print(
+        f"{destination_blob_name} with contents {contents} uploaded to {bucket_name}."
+    )
+
+def hello_gcs(event, context):
+
+    print('Event ID: {}'.format(context.event_id))
+    print('Event type: {}'.format(context.event_type))
+    print('Bucket: {}'.format(event['bucket']))
+    print('File: {}'.format(event['name']))
+    print('Metageneration: {}'.format(event['metageneration']))
+    print('Created: {}'.format(event['timeCreated']))
+    print('Updated: {}'.format(event['updated']))
+
